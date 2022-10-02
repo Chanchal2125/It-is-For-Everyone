b5acdd112af1a4f94fed96adcbbe9832ad83650d
Dhanshri Borse <83999940+Dhanshriborse@users.noreply.github.com>
2022-10-02 19:43:28 +0530

    Update Tower_Of_Hanoi.java

---
+++ Java/Tower_Of_Hanoi.java
@@ -0,0 +1,33 @@
+public class TowerOfHanoiExample
+ {
+
+ // A recursive method to find the solution of Tower of Hanoi
+ static void twrOfHanoi(int disk, char fromPole, char toPole, char auxPole)
+ {
+    
+     if (disk == 1)
+     {
+           System.out.println("Moving disk 1 from pole " + fromPole + " to pole " + toPole);
+            return;
+      }
+      // The first recursive call             
+      
+      twrOfHanoi(disk - 1, fromPole, auxPole, toPole);
+
+       
+       System.out.println("Moving disk " + disk + " from pole " + fromPole + " to pole " + toPole);
+       // The second recursive call
+      
+       twrOfHanoi(disk - 1, auxPole, toPole, fromPole);
+ }
+       
+        public static void main(String argvs[])
+        {
+                int disks = 3; // total number of disks
+                char firstPole = 'A'; // first pole
+                char secondPole = 'B'; // second pole
+                char thirdPole = 'C'; // third pole
+                // invoking the method twrOfHanoi
+                twrOfHanoi( disks, firstPole, thirdPole, secondPole );
+       }
+ } 
