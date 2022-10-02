6331c7c4d3cafb0ddfce872dafff9822ed8fc479
Devtanu Misra <67796502+devtanumisra@users.noreply.github.com>
2022-10-02 16:03:50 +0530

    Add files via upload

---
+++ C++/array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct student{
+    string name;
+    int age;
+    string dept;
+    int year;
+};
+#define stu struct student
+
+int main()
+{
+    stu s1,s2;
+    int n;
+    cout<<"Number of inputs:";
+    cin>>n;
+    return 0;
+}
